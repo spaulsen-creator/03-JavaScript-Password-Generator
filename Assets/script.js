c7f74ce1a6a6cf301eb8c5aa5ba98a4919d5d504
Shawn Paulsen <shawnpaulsen@Shawns-MacBook-Pro.local>
2021-03-05 20:02:28 -0700

    set variables

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,15 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var specialChar = ['!', '@', '#', '$', '%', '&', '?', '*', '^'];
+
+var upperCase = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M'];
+
+var lowerCase = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
