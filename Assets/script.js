e1419c309a34e9e59b9387e2d5838bc344228f85
Shawn Paulsen <shawnpaulsen@Shawns-MacBook-Pro.local>
2021-03-07 08:55:54 -0700

    added password selctions

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+// Assignment Code 
 var generateBtn = document.querySelector("#generate");
 
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -10,14 +10,51 @@ var upperCase = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D'
 var lowerCase = ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'];
 
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+function generatePassword() {
+  // parseInt() function analyzes strings and gives a integer
+  // Determines the lebgth of the password to generate
+  // Prompts within generatePassword function
+  var userLength = parseInt(window.prompt("How many characters do you want your password to be? (8-128)" ));
+  if ( isNaN(userLength) || userLength < 8 || userLength > 128) {
+    window.alert("Choose a password length between 8 and 128")
+    return
+  }
+
+  var AllowedChars = []
+
+  var useLowerCase = window.prompt("Would you like to include lowercase letters? (Y/N".toUpperCase)
+  if (useLowerCase === 'Y') {
+    allowedChars = allowedChars.concat(lowerCase)
+  }
+
+  var useUpperCase = window.prompt("Would you like to include uppercase letters? (Y/N".toUpperCase)
+  if (useUpperCase === 'Y') {
+    allowedChars = allowedChars.concat(upperCase)
+  }
+
+  var useNumbers = window.prompt("Would you like to include numbers? (Y/N".toUpperCase)
+  if (useNumbers === 'Y') {
+    allowedChars = allowedChars.concat(numbers)
+  }
+
+  var useSpecialChar = window.prompt("Would you like to include special characters? (Y/N".toUpperCase)
+  if (useSpecialChar === 'Y') {
+    allowedChars = allowedChars.concat(specialChar)
+  }
+
+  if (allowedChars === 0) {
+    window.alert("You must select at least one to receive a password.")
+    return allowedChars;
+  }
+
+  //var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
+
+  //passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//generateBtn.addEventListener("click", writePassword);
+console.log("hello")
